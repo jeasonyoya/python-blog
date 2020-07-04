e643d2e3d4997f60a3d633689441bb15aaae44fa
jeasonyoya <712411889@qq.com>
2020-07-05 01:58:51 +0800

    2020/07/05 博客第一版

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(r'', views.blog_list, name='blog_list'),
+    path(r'<int:article_id>/', views.blog_detail, name='blog_detail'),
+]
